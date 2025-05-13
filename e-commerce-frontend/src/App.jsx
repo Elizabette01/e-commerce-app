4c0534ec51d17c4a01c56431204e0c583bb80f5c
Elizabette N <ndukaelizabeth26@yahoo.com>
2025-05-13 23:28:59 +0100

    Pages and Routes Created

--- e-commerce-frontend/src/App.jsx
+++ e-commerce-frontend/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Cart from './Pages/cart';
+import PlaceOrder from './Pages/PlaceOrder';
+import Products from './Pages/Products';
+import Order from './Pages/Order';
+import Login from './Pages/login';
+import Categories from './Pages/Categories';
+import Contact from './Pages/contact';
 
 const App = () => {
   return (
     <div>
-      Hello
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/categories' element={<Categories />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/order' element={<Order />} />
+        <Route path='/product' element={<Products />} />
+        <Route path='/placeorder' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
