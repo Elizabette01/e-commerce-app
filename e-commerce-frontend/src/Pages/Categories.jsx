148e5d7ccbeb1a02c7777d2c5e0d732b6d9d6d5e
Elizabette N <ndukaelizabeth26@yahoo.com>
2025-05-17 17:16:08 +0100

    categories section

--- e-commerce-frontend/src/Pages/Categories.jsx
+++ e-commerce-frontend/src/Pages/Categories.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Categories = () => {
+
+  const { products } = useContext(ShopContext);
+
   return (
-    <div>
+    <div className='flex flex-col sm:flex-row gap1 sm:gap-10 pt-10 border-t'>
       
     </div>
   )
