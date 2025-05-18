49397bc15d06186d00c973fa35a91b89082c4f8f
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-05-18 13:16:32 +0100

    test

--- e-commerce-frontend/src/Pages/Categories.jsx
+++ e-commerce-frontend/src/Pages/Categories.jsx
@@ -3,7 +3,7 @@ import { ShopContext } from '../context/ShopContext'
 
 const Categories = () => {
 
-  const { products } = useContext(ShopContext);
+  // const { products } = useContext(ShopContext);
 
   return (
     <div className='flex flex-col sm:flex-row gap1 sm:gap-10 pt-10 border-t'>
