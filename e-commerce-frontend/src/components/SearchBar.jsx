e0564a633d35d84d854c3b2fd5e46a36fb267a07
Elizabeth Nduka <ndukaelizabeth26@gmail.com>
2025-05-20 07:52:28 +0100

    search bar component updated

--- e-commerce-frontend/src/components/SearchBar.jsx
+++ e-commerce-frontend/src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
 import { FaSearch } from "react-icons/fa";
 import { RiCloseLargeLine  } from "react-icons/ri";
@@ -7,14 +7,20 @@ import { useLocation } from 'react-router-dom';
 const SearchBar = () => {
 
   const { search, setSearch, showSearch, setShowSearch} = useContext(ShopContext);
+  const [visible, setVisible] = useState(false)
 
   const location = useLocation();
 
   useEffect(()=> {
+    if (location.pathname.includes('collection')) {
+      setVisible(true)
+    }
+    else {
+      setVisible(false)
+    }
+  }, [location])
 
-  }, [])
-
-  return showSearch ? (
+  return showSearch && visible ? (
     <div className='bg-gray-50 text-center'>
       <div className="inline-flex items-center justify-center border border-gray-400 rounded-full px-5 py-2 my-5 mx-auto w-3/4 sm:w-1/2">
         <input type="text" className="flex-1 outline-none bg-inherit text-sm placeholder:text-gray-400" placeholder='Search  Product' onChange={(e) => setSearch(e.target.value)} />
